f37019a515e11a54a11ae3b9233a2c6b2799e59f
Florian Weich <hallo@florianweich.me>
2017-02-14 20:44:04 +0100

    Initial Commit

---
+++ Simple Time Tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Simple Time Tracker
+//
+//  Created by Florian Weich on 14.02.17.
+//  Copyright © 2017 Florian Weich. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Simple Time Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simple Time Tracker
+//
+//  Created by Florian Weich on 14.02.17.
+//  Copyright © 2017 Florian Weich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
